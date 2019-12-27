7f69da97177acb80519d60f90d80de7665e8ceb3
AguacateContreras <antonio.contreras.arzate@gmail.com>
2019-12-27 01:42:01 -0600

    Using paths

--- IMGHelper.php
+++ IMGHelper.php
@@ -1,8 +1,12 @@
 <?php
   class IMGHelper{
     public function __construct($path){
-
+      $this->path = $path;
     }
-    
+
+
+    private function use_path($file){
+      return preg_replace("/\/*$/", "", $this->path)."/".$file;
+    }
   }
 ?>
